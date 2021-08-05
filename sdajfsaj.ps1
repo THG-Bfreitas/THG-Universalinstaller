267193f8bafb03879b63d11f514a8e723b7fa984
THG-Bfreitas <87115315+THG-Bfreitas@users.noreply.github.com>
2021-08-05 13:02:18 -0700

    Create sdajfsaj.ps1

---
+++ sdajfsaj.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest -Uri https://appriver3651012518.sharepoint.com/:u:/s/HilbInfrastructureTeam/EQzJYNN8kK1OnQD9A2lN2JABM8meY6__Xt7e7_7YXrazsw?download=1 -OutFile C:\hilb\QualysCloudAgent.exe
